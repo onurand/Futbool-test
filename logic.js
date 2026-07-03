99bf002bee32275210f1eaebeeeea4a47bdce791
onurand <onurand@users.noreply.github.com>
2026-07-03 11:13:42 +0000

    Deploy Yarn Loop a3b14d7d7e94395caa402fd67b446a6e17bd70d0

--- logic.js
+++ logic.js
@@ -20,89 +20,379 @@
   ];
 
   var PATTERNS = [
-    { name: 'kalp', rows: [
-      '............',
-      '..AA....AA..',
-      '.AAAA..AAAA.',
-      'AAAAAAAAAAAA',
-      'AAAAAAAAAAAA',
-      'AAAAAAAAAAAA',
-      '.AAAAAAAAAA.',
-      '..AAAAAAAA..',
-      '...AAAAAA...',
-      '....AAAA....',
-      '.....AA.....',
-      '............'
+    { name: 'kalp', colors: { '.': '#fff3e0', 'A': '#ff5677' }, rows: [
+      '..............',
+      '...AA....AA...',
+      '..AAAA..AAAA..',
+      '.AAAAAAAAAAAA.',
+      '.AAAAAAAAAAAA.',
+      '.AAAAAAAAAAAA.',
+      '..AAAAAAAAAA..',
+      '...AAAAAAAA...',
+      '....AAAAAA....',
+      '.....AAAA.....',
+      '......AA......',
+      '..............'
     ] },
-    { name: 'gül', rows: [
-      '............',
-      '....AAAA....',
-      '...AAAAAA...',
-      '..AAAAAAAA..',
-      '..AAAAAAAA..',
-      '...AAAAAA...',
-      '....AAAA....',
-      '.B...BB...B.',
-      '.BB..BB..BB.',
-      '..BBBBBBBB..',
-      '.....BB.....',
-      '.....BB.....'
+    { name: 'yıldız', colors: { '.': '#5f7cff', 'A': '#ffd93d' }, rows: [
+      '..............',
+      '......AA......',
+      '......AA......',
+      '.....AAAA.....',
+      '.AAAAAAAAAAAA.',
+      '..AAAAAAAAAA..',
+      '...AAAAAAAA...',
+      '....AAAAAA....',
+      '....AAAAAA....',
+      '...AAA..AAA...',
+      '..AA......AA..',
+      '..............'
     ] },
-    { name: 'civciv', rows: [
-      '............',
-      '.....CCC....',
-      '....CCCCC...',
-      '...AAAAAAA..',
-      '..AAAADAAA..',
-      '..AAAAAAAA..',
-      '.AAAAAAAAAA.',
-      '.AAABBAAAAA.',
-      '..AABBBAAA..',
-      '...AAAAAA...',
-      '....CC.CC...',
-      '............'
+    { name: 'ay', colors: { '.': '#5f7cff', 'A': '#ffd93d', 'B': '#fff3e0' }, rows: [
+      '..............',
+      '.....AAAA.....',
+      '....AAAA...B..',
+      '...AAAA.......',
+      '...AAA....B...',
+      '...AAA........',
+      '...AAA....B...',
+      '...AAAA.......',
+      '....AAAA..B...',
+      '.....AAAA.....',
+      '..............'
     ] },
-    { name: 'kiraz', rows: [
-      '............',
-      '......BB....',
-      '.....BB.....',
-      '....BB.B....',
-      '...BB...B...',
-      '..AAA...AAA.',
-      '.AAAAA.AAAAA',
-      '.AAAAA.AAAAA',
-      '.AAAAA.AAAAA',
-      '..AAA...AAA.',
-      '............',
-      '............'
+    { name: 'balık', colors: { '.': '#6cbdf2', 'A': '#ff9f2e', 'B': '#5a4a52' }, rows: [
+      '..............',
+      '....AAAAA...A.',
+      '..AAAAAAAA.AA.',
+      '.AAAABAAAAAAA.',
+      '.AAAAAAAAAAAA.',
+      '..AAAAAAAA.AA.',
+      '....AAAAA...A.',
+      '..............'
     ] },
-    { name: 'yıldız', rows: [
-      '............',
-      '.....AA.....',
-      '.....AA.....',
-      '....AAAA....',
-      'AAAAAAAAAAAA',
-      '.AAAAAAAAAA.',
-      '..AAAAAAAA..',
-      '...AAAAAA...',
-      '...AAAAAA...',
-      '..AAA..AAA..',
-      '.AA......AA.',
-      '............'
+    { name: 'kiraz', colors: { '.': '#fff3e0', 'A': '#ff5677', 'B': '#4cd471' }, rows: [
+      '..............',
+      '.......BB.....',
+      '......BB......',
+      '.....BB.B.....',
+      '....BB...B....',
+      '...AAA...AAA..',
+      '..AAAAA.AAAAA.',
+      '..AAAAA.AAAAA.',
+      '..AAAAA.AAAAA.',
+      '...AAA...AAA..',
+      '..............'
     ] },
-    { name: 'ev', rows: [
-      '............',
-      '.....BB.....',
-      '....BBBB....',
-      '...BBBBBB...',
-      '..BBBBBBBB..',
-      '.BBBBBBBBBB.',
-      '..AAAAAAAA..',
-      '..AAADDAAA..',
-      '..AAADDAAA..',
-      '..AAADDAAA..',
-      '..AAAAAAAA..',
-      '............'
+    { name: 'elma', colors: { '.': '#fff3e0', 'A': '#ff5677', 'B': '#4cd471' }, rows: [
+      '..............',
+      '.......BB.....',
+      '......BB......',
+      '....AAAAAA....',
+      '...AAAAAAAA...',
+      '..AAAAAAAAAA..',
+      '..AAAAAAAAAA..',
+      '..AAAAAAAAAA..',
+      '...AAAAAAAA...',
+      '....AA..AA....',
+      '..............'
+    ] },
+    { name: 'balon', colors: { '.': '#6cbdf2', 'B': '#ff5677', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '....BBBBBB....',
+      '...BBBBBBBB...',
+      '...BBBBBBBB...',
+      '...BBBBBBBB...',
+      '....BBBBBB....',
+      '.....BBBB.....',
+      '......BB......',
+      '......D.......',
+      '.....D........',
+      '......D.......',
+      '..............'
+    ] },
+    { name: 'güneş', colors: { '.': '#6cbdf2', 'B': '#ffd93d' }, rows: [
+      '..............',
+      '..B...BB...B..',
+      '.....BBBB.....',
+      '..B.BBBBBB.B..',
+      '....BBBBBB....',
+      '..B.BBBBBB.B..',
+      '.....BBBB.....',
+      '..B...BB...B..',
+      '..............'
+    ] },
+    { name: 'fiyonk', colors: { '.': '#fff3e0', 'B': '#ff7ad9', 'D': '#c168ff' }, rows: [
+      '..............',
+      '..BB......BB..',
+      '.BBBB....BBBB.',
+      '.BBBBB..BBBBB.',
+      '.BBBBBDDBBBBB.',
+      '.BBBBBDDBBBBB.',
+      '.BBBBB..BBBBB.',
+      '.BBBB....BBBB.',
+      '..BB......BB..',
+      '..............'
+    ] },
+    { name: 'elmas', colors: { '.': '#fff3e0', 'B': '#35c7f4', 'A': '#8fd6f7' }, rows: [
+      '..............',
+      '....BBBBBB....',
+      '...BABBBBAB...',
+      '..BBBBBBBBBB..',
+      '...BBBBBBBB...',
+      '....BBBBBB....',
+      '.....BBBB.....',
+      '......BB......',
+      '..............'
+    ] },
+    { name: 'futbol topu', colors: { '.': '#4cd471', 'A': '#fff9ee', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '....AAAAAA....',
+      '..AAAADDAAAA..',
+      '.AAAAADDAAAAA.',
+      '.AADAAAAAADAA.',
+      '.AADAAAAAADAA.',
+      '.AAAAADDAAAAA.',
+      '..AAAADDAAAA..',
+      '....AAAAAA....',
+      '..............'
+    ] },
+    { name: 'ev', colors: { '.': '#6cbdf2', 'A': '#fff3e0', 'B': '#ff5677', 'D': '#ff9f2e' }, rows: [
+      '..............',
+      '......BB......',
+      '.....BBBB.....',
+      '....BBBBBB....',
+      '...BBBBBBBB...',
+      '..BBBBBBBBBB..',
+      '...AAAAAAAA...',
+      '...AAADDAAA...',
+      '...AAADDAAA...',
+      '...AAAAAAAA...',
+      '..............'
+    ] },
+    { name: 'civciv', colors: { '.': '#6cbdf2', 'A': '#ffd93d', 'B': '#ff9f2e', 'C': '#ff9f2e', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '......CCC.....',
+      '.....CCCCC....',
+      '....AAAAAAA...',
+      '...AAAADAAA...',
+      '...AAAAAAAA...',
+      '..AAAAAAAAAA..',
+      '..AAABBAAAAA..',
+      '...AABBBAAA...',
+      '....AAAAAA....',
+      '.....CC.CC....',
+      '..............'
+    ] },
+    { name: 'dondurma', colors: { '.': '#fff3e0', 'B': '#ff7ad9', 'D': '#fff9ee', 'A': '#e0a35c' }, rows: [
+      '..............',
+      '.....BBBB.....',
+      '....BBBBBB....',
+      '....DDDDDD....',
+      '....DDDDDD....',
+      '.....AAAA.....',
+      '.....AAAA.....',
+      '......AA......',
+      '......AA......',
+      '..............'
+    ] },
+    { name: 'kuş', colors: { '.': '#8fd6f7', 'A': '#35c7f4', 'B': '#5a4a52', 'D': '#ff9f2e' }, rows: [
+      '..............',
+      '.....AAAA.....',
+      '....AABAAA....',
+      '.DDAAAAAAA....',
+      '....AAAAAAAA..',
+      '....AAAAAA....',
+      '.....AAAA.....',
+      '......AA......',
+      '.....D.D......',
+      '..............'
+    ] },
+    { name: 'tavşan', colors: { '.': '#6cbdf2', 'A': '#fff3e0', 'B': '#5a4a52', 'D': '#ff7ad9' }, rows: [
+      '...AA....AA...',
+      '...AA....AA...',
+      '...AA....AA...',
+      '..AAAAAAAAAA..',
+      '.AAAAAAAAAAAA.',
+      '.AABBAAAABBAA.',
+      '.AAAAAAAAAAAA.',
+      '.AAAAADDAAAAA.',
+      '..AAAAAAAAAA..',
+      '...AAAAAAAA...',
+      '..............'
+    ] },
+    { name: 'kedi', colors: { '.': '#6cbdf2', 'A': '#ff9f2e', 'B': '#5a4a52', 'D': '#ff7ad9' }, rows: [
+      '..............',
+      '..AA......AA..',
+      '..AAA....AAA..',
+      '..AAAAAAAAAA..',
+      '.AAAAAAAAAAAA.',
+      '.AABBAAAABBAA.',
+      '.AAAAAAAAAAAA.',
+      '.AAAADAADAAAA.',
+      '..AAAADDAAAA..',
+      '...AAAAAAAA...',
+      '..............'
+    ] },
+    { name: 'köpek', colors: { '.': '#6cbdf2', 'A': '#d8a35f', 'B': '#8a5a36', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '.BB........BB.',
+      '.BBB......BBB.',
+      '.BBAAAAAAAABB.',
+      '..AAAAAAAAAA..',
+      '..ADAAAAAADA..',
+      '..AAAAAAAAAA..',
+      '..AAAADDAAAA..',
+      '...AAADDAAA...',
+      '....AAAAAA....',
+      '..............'
+    ] },
+    { name: 'kurbağa', colors: { '.': '#6cbdf2', 'A': '#4cd471', 'B': '#5a4a52', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '..AA......AA..',
+      '.AABA....ABAA.',
+      '.AAAAAAAAAAAA.',
+      '.AAAAAAAAAAAA.',
+      '..AADAAAADAA..',
+      '..AAADDDDAAA..',
+      '...AAAAAAAA...',
+      '..............'
+    ] },
+    { name: 'gül', colors: { '.': '#fff3e0', 'A': '#ff5677', 'B': '#4cd471' }, rows: [
+      '..............',
+      '.....AAAA.....',
+      '....AAAAAA....',
+      '...AAAAAAAA...',
+      '...AAAAAAAA...',
+      '....AAAAAA....',
+      '.....AAAA.....',
+      '.B....BB....B.',
+      '.BB...BB...BB.',
+      '..BBBBBBBBBB..',
+      '......BB......',
+      '..............'
+    ] },
+    { name: 'mantar', colors: { '.': '#fff3e0', 'B': '#ff5677', 'A': '#fff9ee', 'D': '#f0d9b8' }, rows: [
+      '..............',
+      '....BBBBBB....',
+      '..BBAABBAABB..',
+      '.BBAABBBBAABB.',
+      '.BBBBBBBBBBBB.',
+      '.BBAABBBBAABB.',
+      '..BBBBBBBBBB..',
+      '...DDDDDDDD...',
+      '...DDBDDBDD...',
+      '...DDDDDDDD...',
+      '..............'
+    ] },
+    { name: 'cupcake', colors: { '.': '#6cbdf2', 'B': '#ff7ad9', 'D': '#ff5677', 'A': '#e0a35c' }, rows: [
+      '..............',
+      '......BB......',
+      '....BBBBBB....',
+      '...BBBBBBBB...',
+      '..DDDDDDDDDD..',
+      '...AAAAAAAA...',
+      '...AAAAAAAA...',
+      '...AAAAAAAA...',
+      '....AAAAAA....',
+      '..............'
+    ] },
+    { name: 'ördek', colors: { '.': '#6cbdf2', 'B': '#ffd93d', 'A': '#5a4a52', 'D': '#ff9f2e' }, rows: [
+      '..............',
+      '........BBBB..',
+      '.......BBABB..',
+      '.....DDBBBB...',
+      '.......BBBB...',
+      '..BBBBBBBBB...',
+      '..BBBBBBBBB...',
+      '...BBBBBBB....',
+      '....BBBBB.....',
+      '..............'
+    ] },
+    { name: 'uğur böceği', colors: { '.': '#5ecb84', 'A': '#ff5677', 'B': '#5a4a52' }, rows: [
+      '..............',
+      '.....BBBB.....',
+      '....BBBBBB....',
+      '..AAAABBAAAA..',
+      '.AABAABBAABAA.',
+      '.AAAAABBAAAAA.',
+      '.AABAABBAABAA.',
+      '..AAAABBAAAA..',
+      '...AAABBAAA...',
+      '..............'
+    ] },
+    { name: 'penguen', colors: { '.': '#6cbdf2', 'B': '#5a4a52', 'A': '#fff9ee', 'D': '#ff9f2e' }, rows: [
+      '..............',
+      '....BBBBBB....',
+      '...BBBBBBBB...',
+      '...BABBBBAB...',
+      '...BBBDDBBB...',
+      '..BBAAAAAABB..',
+      '..BBAAAAAABB..',
+      '..BBAAAAAABB..',
+      '...BAAAAAAB...',
+      '...DD....DD...',
+      '..............'
+    ] },
+    { name: 'panda', colors: { '.': '#5ecb84', 'A': '#fff9ee', 'B': '#5a4a52', 'D': '#5a4a52' }, rows: [
+      '..............',
+      '..BB......BB..',
+      '..AAAAAAAAAA..',
+      '.AAAAAAAAAAAA.',
+      '.ABBAAAAAABBA.',
+      '.ABBAAAAAABBA.',
+      '.AAAAADDAAAAA.',
+      '..AAAADDAAAA..',
+      '...AAAAAAAA...',
+      '..............'
+    ] },
+    { name: 'roket', colors: { '.': '#5f7cff', 'A': '#fff3e0', 'B': '#ff5677', 'D': '#ff9f2e' }, rows: [
+      '..............',
+      '......AA......',
+      '.....AAAA.....',
+      '.....ADDA.....',
+      '.....ADDA.....',
+      '.....AAAA.....',
+      '....BAAAAB....',
+      '...BBAAAABB...',
+      '...BB.AA.BB...',
+      '......DD......',
+      '..............'
+    ] },
+    { name: 'taç', colors: { '.': '#b591f2', 'B': '#ffd93d', 'D': '#ff5677' }, rows: [
+      '..............',
+      '..B...BB...B..',
+      '..BB.BBBB.BB..',
+      '..BBBBBBBBBB..',
+      '..BBDBBBBDBB..',
+      '..BBBBBBBBBB..',
+      '..............'
+    ] },
+    { name: 'yumak', colors: { '.': '#fff3e0', 'A': '#ff5677', 'B': '#ff92ac' }, rows: [
+      '..............',
+      '....AAAAAA....',
+      '..AABBBBBBAA..',
+      '.AABAAAAAABAA.',
+      '.ABAABBBBAABA.',
+      '.ABABAAAABABA.',
+      '.AABAABBAABAA.',
+      '..AABBAABBAA..',
+      '....AAAAAA....',
+      '..........A...',
+      '..............'
+    ] },
+    { name: 'çiçek', colors: { '.': '#6cbdf2', 'B': '#ff7ad9', 'D': '#ffd93d', 'A': '#4cd471', 'C': '#4cd471' }, rows: [
+      '..............',
+      '....BB..BB....',
+      '...BBBBBBBB...',
+      '...BBBDDBBB...',
+      '...BBBDDBBB...',
+      '....BBBBBB....',
+      '......AA......',
+      '...C..AA..C...',
+      '....CCAACC....',
+      '......AA......',
+      '..............'
     ] }
   ];
 
@@ -118,7 +408,7 @@
 
   function generateLevel(n) {
     var rnd = mulberry32(n * 7919 + 271);
-    var pattern = PATTERNS[Math.floor(rnd() * PATTERNS.length)];
+    var pattern = PATTERNS[(n - 1) % PATTERNS.length];
     var rows = pattern.rows.length, cols = pattern.rows[0].length;
 
     var roles = ['.'];
@@ -128,12 +418,17 @@
       });
     });
 
-    var colors = PALETTE.slice();
-    for (var i = colors.length - 1; i > 0; i--) {
+    // desen sabit renk tanımlıyorsa onu kullan (panda siyah-beyaz kalsın),
+    // tanımsız roller karıştırılmış paletten tamamlanır
+    var shuffled = PALETTE.slice();
+    for (var i = shuffled.length - 1; i > 0; i--) {
       var j = Math.floor(rnd() * (i + 1));
-      var tmp = colors[i]; colors[i] = colors[j]; colors[j] = tmp;
+      var tmp = shuffled[i]; shuffled[i] = shuffled[j]; shuffled[j] = tmp;
     }
-    colors = colors.slice(0, roles.length);
+    var colors = roles.map(function (ch, ri) {
+      if (pattern.colors && pattern.colors[ch]) return pattern.colors[ch];
+      return shuffled[ri % shuffled.length];
+    });
 
     var grid = pattern.rows.map(function (row) {
       return row.split('').map(function (ch) { return roles.indexOf(ch); });
